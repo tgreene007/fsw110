44b66bc5117b257ac76d8c2faf4673d01d830c31
tgreene007 <90284995+tgreene007@users.noreply.github.com>
2021-11-22 10:49:46 -0800

    Create basicwebsite.js

---
+++ Week-2/project1/basicwebsite.js
@@ -0,0 +1,31 @@
+var navOne = document.createElement('a')
+navOne.setAttribute('href', '');
+navOne.setAttribute('id', 'navOne')
+navOne.textContent = 'navOne | ';
+document.body.appendChild(navOne)
+
+var navTwo = document.createElement('a')
+navTwo.setAttribute('href', '');
+navTwo.setAttribute('id', 'navTwo')
+navTwo.textContent = 'navTwo | ';
+document.body.appendChild(navTwo);
+
+var navThree = document.createElement('a')
+navThree.setAttribute('href', '');
+navThree.setAttribute('id', 'navThree')
+navThree.textContent = 'navThree | ';
+document.body.appendChild(navThree)
+
+var h1One = document.createElement('h1');
+h1One.textContent = 'this is a web header';
+h1One.setAttribute('id', 'h1js');
+document.body.appendChild(h1One);
+
+var pOne= document.createElement('p');
+pOne.textContent = 'This is a basic paragragh';
+pOne.setAttribute('id', 'pjs');
+document.body.appendChild(pOne);
+
+var fOne = document.createElement('footer');
+fOne.setAttribute('id', 'f1js');
+document.body.appendChild(fOne);
